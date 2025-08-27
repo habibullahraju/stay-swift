7097e274bbee159d03cc16f3f7e21bd4e8afd245
habibullahraju <raju.ra423@gmail.com>
2025-08-27 21:24:12 +0600

    first commit

---
+++ app/(auth)/layout.js
@@ -0,0 +1,34 @@
+import Navbar from "@/components/Navbar";
+import { dbConnect } from "@/service/mongo";
+import localFont from "next/font/local";
+import "../globals.css";
+
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "Stay swift",
+  description: "One place stop for hosptability",
+};
+
+export default async function RootLayout({ children }) {
+  await dbConnect();
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar sideMenu={false} />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ app/(home)/bookings/page.js
@@ -0,0 +1,29 @@
+import { auth } from "@/auth";
+import PastBooking from "@/components/user/booking/PastBooking";
+import UpcomingBooking from "@/components/user/booking/UpcomingBooking";
+import ProfileInfo from "@/components/user/ProfileInfo";
+import { redirect } from "next/navigation";
+
+export default async function BookingsPage() {
+  const sesstion = await auth();
+  if (!sesstion) {
+    redirect("/login");
+  }
+  return (
+    <>
+      <section className="mt-[100px]">
+        <div className="container">
+          <ProfileInfo />
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+            <PastBooking />
+            <UpcomingBooking />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
